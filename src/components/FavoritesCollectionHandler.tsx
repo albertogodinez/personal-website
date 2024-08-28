6448bb78f11bd2fcab1f6df821546fcaceb2ecdd
albertogodinez <alberto.godinez90@gmail.com>
2024-08-28 16:12:33 -0500

    updates name of components

--- src/components/FavoritesCollectionHandler.tsx
+++ src/components/FavoritesCollectionHandler.tsx
@@ -1,32 +1,17 @@
-import { Cloudinary } from '@cloudinary/url-gen';
-import { scale } from '@cloudinary/url-gen/actions/resize';
-
 import { type FavoritesCollection } from '../content/config';
-import CardPreview from './CardPreview';
+import { selectedFavoriteType, selectedYear } from '../functionalityStore';
+import ImageHandler from './ImageHandler';
 import './styles.css';
 
 export interface FavoritesCollectionHandlerProps {
   collection: FavoritesCollection;
 }
 
-const cld = new Cloudinary({
-  cloud: {
-    cloudName: import.meta.env.PUBLIC_CLOUDINARY_CLOUD_NAME
-  }
-});
-
 const FavoritesCollectionHandler: React.FC<FavoritesCollectionHandlerProps> = ({ collection }) => {
   return (
     <div className="grid">
       {collection.map((favorite: any, index: number) => (
-        <CardPreview key={index} title={favorite.data.title}>
-          <img
-            key={index}
-            className="image"
-            src={`${cld.image(favorite.data.imageId).quality('auto').format('auto').resize(scale().width(1000)).toURL()}`}
-            alt="card preview"
-          />
-        </CardPreview>
+        <ImageHandler className="image" key={index} title={favorite.data.title} imageId={favorite.data.imageId} />
       ))}
     </div>
   );
--- src/functionalityStore.ts
+++ src/functionalityStore.ts
@@ -2,10 +2,10 @@ import { type WritableAtom, atom } from 'nanostores';
 
 import { FAVORITE_TYPES } from './constants/memorabilia';
 
-export const isDialogOpen: WritableAtom<boolean> = atom(false);
-export const selectedYear: WritableAtom<string> = atom('');
-export const selectedFavoriteType: WritableAtom<FAVORITE_TYPES | string> = atom('');
 export const currentSelectionStore = atom(undefined);
+export const isDialogOpen: WritableAtom<boolean> = atom(false);
+export const selectedFavoriteType: WritableAtom<FAVORITE_TYPES | string> = atom('');
+export const selectedYear: WritableAtom<string> = atom('');
 export const storeMap: WritableAtom<Map<string, WritableAtom<string>>> = atom(
   new Map([
     ['SELECTED_YEAR', selectedYear],
