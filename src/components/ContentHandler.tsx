df648343ac6f020e492a5166bdd69439b2010de1
albertogodinez <alberto.godinez90@gmail.com>
2024-08-30 21:57:55 -0500

    fixes layout of images and overlay

--- src/components/ContentHandler.tsx
+++ src/components/ContentHandler.tsx
@@ -19,23 +19,23 @@ const ContentHandler: React.FC<ImageHandlerProps> = ({ title, description, image
   const [isContentInfoVisible, setContentInfoVisibility] = useState(false);
 
   const handleClick = () => {
-    console.log('title');
     setContentInfoVisibility(!isContentInfoVisible);
   };
 
   return (
-    <div className="content">
-      <div className="content-info" hidden aria-hidden="true">
+    <div className="content" onClick={handleClick} onFocus={handleClick} onBlur={handleClick}>
+      <div className={`content-info ${isContentInfoVisible ? 'visible' : ''}`} aria-hidden={!isContentInfoVisible}>
         <h3 className="content-info__title">{title}</h3>
         <p className="content-info__desc">{description}</p>
-        <div className="content-info__overlay" hidden aria-hidden="true" />
+        <div className="content-info__overlay" aria-hidden={!isContentInfoVisible} />
       </div>
       <img
-        className={className}
+        className="image"
         src={`${cld.image(imageId).quality('auto').format('auto').resize(scale().width(1000)).toURL()}`}
         alt="card preview"
       />
     </div>
   );
 };
+
 export default ContentHandler;
