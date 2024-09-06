c1258fa5d9470a7755174502a34cd984b76204b9
albertogodinez <alberto.godinez90@gmail.com>
2024-09-05 23:30:37 -0500

    moves gradient into its own component

--- src/components/SelectionHandler.tsx
+++ src/components/SelectionHandler.tsx
@@ -1,12 +1,13 @@
 import { useStore } from '@nanostores/react';
 import * as Dialog from '@radix-ui/react-dialog';
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useState } from 'react';
 
-import SelectWrapper from '../components/SelectWrapper';
 import { SELECTION_TYPES, YEARS } from '../constants/memorabilia';
 import { FAVORITE_TYPES } from '../constants/memorabilia';
 import { selectedFavoriteType, selectedYear } from '../functionalityStore';
 import '../styles/gradient-bg.css';
+import GradientBackground from './GradientBackground';
+import SelectWrapper from './SelectWrapper';
 
 const favoriteTypesArray = Object.values(FAVORITE_TYPES).map((type) => type.toLowerCase());
 
@@ -17,7 +18,6 @@ export const SelectionHandler: React.FC = () => {
   const [activeSelectionStoreKey, setActiveSelectionStoreKey] = useState<SELECTION_TYPES | undefined>(undefined);
   const [activePlaceholder, setActivePlaceholder] = useState<string>('');
   const [isDialogOpen, setIsDialogOpen] = useState<boolean>(false);
-  const [interBubble, setInterBubble] = useState<HTMLDivElement | null>(null);
 
   const handleActiveSelection = (options: string[], selectionStoreKey: SELECTION_TYPES, placeholder: string) => {
     setActiveOptions(options);
@@ -25,52 +25,13 @@ export const SelectionHandler: React.FC = () => {
     setActivePlaceholder(placeholder);
   };
 
-  const interactiveRef = useCallback((node: HTMLDivElement | null) => {
-    if (node !== null) {
-      setInterBubble(node);
-    }
-  }, []);
-
-  useEffect(() => {
-    if (!isDialogOpen || !interBubble) return;
-
-    let curX = 0;
-    let curY = 0;
-    let tgX = 0;
-    let tgY = 0;
-
-    function move() {
-      curX += (tgX - curX) / 20;
-      curY += (tgY - curY) / 20;
-      if (interBubble) {
-        interBubble.style.transform = `translate(${Math.round(curX)}px, ${Math.round(curY)}px)`;
-      }
-      requestAnimationFrame(move);
-    }
-
-    const handleMouseMove = (event: MouseEvent) => {
-      tgX = event.clientX;
-      tgY = event.clientY;
-    };
-
-    window.addEventListener('mousemove', handleMouseMove);
-    move();
-
-    // Cleanup function to remove event listener
-    return () => {
-      console.log('cleanup');
-      window.removeEventListener('mousemove', handleMouseMove);
-    };
-  }, [isDialogOpen, interBubble]);
-
-  // TODO: Separate the gradient background into a separate component
   return (
     <Dialog.Root open={isDialogOpen} onOpenChange={setIsDialogOpen}>
       <Dialog.Trigger asChild>
         <h1>
           my favorite{' '}
           <span
-            style={{ cursor: 'pointer', fontWeight: 'bold' }}
+            style={{ cursor: 'pointer' }}
             onClick={() =>
               handleActiveSelection(
                 favoriteTypesArray,
@@ -83,7 +44,7 @@ export const SelectionHandler: React.FC = () => {
           </span>
           of
           <span
-            style={{ cursor: 'pointer', fontSize: '2rem' }}
+            style={{ cursor: 'pointer' }}
             onClick={() => handleActiveSelection(YEARS, SELECTION_TYPES.SELECTED_YEAR, $selectedYear || 'all time')}
           >{` ${$selectedYear || 'all time'}`}</span>
         </h1>
@@ -97,30 +58,7 @@ export const SelectionHandler: React.FC = () => {
             selectionStoreKey={activeSelectionStoreKey}
             onSelectionMade={() => setIsDialogOpen(!isDialogOpen)}
           />
-          <div className="gradient-bg">
-            <svg xmlns="http://www.w3.org/2000/svg">
-              <defs>
-                <filter id="goo">
-                  <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
-                  <feColorMatrix
-                    in="blur"
-                    mode="matrix"
-                    values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -8"
-                    result="goo"
-                  />
-                  <feBlend in="SourceGraphic" in2="goo" />
-                </filter>
-              </defs>
-            </svg>
-            <div className="gradients-container">
-              <div className="g1"></div>
-              <div className="g2"></div>
-              <div className="g3"></div>
-              <div className="g4"></div>
-              <div className="g5"></div>
-              <div className="interactive" ref={interactiveRef}></div>
-            </div>
-          </div>
+          <GradientBackground />
         </Dialog.Content>
       </Dialog.Portal>
     </Dialog.Root>
