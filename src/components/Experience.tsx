436a4c05b732dd86b1874614ecdc80c6439d3eae
albertogodinez <alberto.godinez90@gmail.com>
2024-09-22 01:32:42 -0500

    adds first version of the experience accordion

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,6 +1,6 @@
 import * as Accordion from '@radix-ui/react-accordion';
 
-import type { ExperienceCollection } from '../content/config';
+import type { ExperienceCollection, ExperienceCollectionEntry } from '../content/config';
 import '../styles/experience.css';
 
 export interface ExperienceProps {
@@ -13,71 +13,31 @@ const Experience: React.FC<ExperienceProps> = ({ allExperience, workExperience, 
   console.log('allExperience', allExperience);
   console.log('workExperience', workExperience);
   console.log('projectExperience', projectExperience);
+  //   TODO: Fix order of the accordion items according to the date
   return (
-    <Accordion.Root className="width-screen flex flex-column gap-4" type="single" collapsible>
-      <Accordion.Item className="" value="expedia">
-        <Accordion.Header>
-          <Accordion.Trigger className="flex flex-row justify-between width-screen">
-            <span>Software Engineer @ Expedia </span>
-            <span>2023 - Now</span>
-          </Accordion.Trigger>
-        </Accordion.Header>
-        <Accordion.Content>
-          <p>Enter experience here</p>
-        </Accordion.Content>
-      </Accordion.Item>
-
-      <Accordion.Item value="twitter">
-        <Accordion.Header>
-          <Accordion.Trigger>
-            <span>Twitter</span>
-            <span>Software Engineer</span>
-            <span>2022</span>
-          </Accordion.Trigger>
-        </Accordion.Header>
-        <Accordion.Content>
-          <p>Enter experience here</p>
-        </Accordion.Content>
-      </Accordion.Item>
-
-      <Accordion.Item value="solesavy">
-        <Accordion.Header>
-          <Accordion.Trigger>
-            <span>SoleSavy</span>
-            <span>Software Engineer</span>
-            <span>2021 - 2022</span>
-          </Accordion.Trigger>
-        </Accordion.Header>
-        <Accordion.Content>
-          <p>Enter experience here</p>
-        </Accordion.Content>
-      </Accordion.Item>
-
-      <Accordion.Item value="ibm">
-        <Accordion.Header>
-          <Accordion.Trigger>
-            <span>IBM</span>
-            <span>Software Engineer</span>
-            <span>2020 - 2021</span>
-          </Accordion.Trigger>
-        </Accordion.Header>
-        <Accordion.Content>
-          <p>Enter experience here</p>
-        </Accordion.Content>
-      </Accordion.Item>
-
-      <Accordion.Item value="trs">
-        <Accordion.Header>
-          <Accordion.Trigger>
-            <span>Teacher Retirement System of Texas</span>
-            <span>Software Engineer</span>
-            <span>2017 - 2020</span>
-          </Accordion.Trigger>
-        </Accordion.Header>
-        <Accordion.Content>
-          <p>Enter experience here</p>
-        </Accordion.Content>
-      </Accordion.Item>
+    <Accordion.Root className="width-screen flex flex-column gap-4 experience-accordion" type="single" collapsible>
+      {allExperience.map((experience: ExperienceCollectionEntry) => (
+        <Accordion.Item value={experience.data.company}>
+          <Accordion.Header>
+            <Accordion.Trigger asChild>
+              <div className="flex flex-row justify-between experience-accordion__trigger">
+                <span>
+                  {experience.data.position} @ {experience.data.company}
+                </span>
+                <span>
+                  {experience.data.startDate.toLocaleDateString('en-US', { year: 'numeric' })}-
+                  {experience.data.endDate
+                    ? experience.data.endDate.toLocaleDateString('en-US', { year: 'numeric' })
+                    : 'present'}
+                </span>
+              </div>
+            </Accordion.Trigger>
+          </Accordion.Header>
+          <Accordion.Content>
+            <p>{experience.data.description}</p>
+          </Accordion.Content>
+        </Accordion.Item>
+      ))}
     </Accordion.Root>
   );
 };
