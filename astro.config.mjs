e30eacf79abdce3d9aa1851bc55653e1c41025d7
albertogodinez <alberto.godinez90@gmail.com>
2024-10-13 23:51:55 -0500

    testing out nav issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,6 @@ export default defineConfig({
   integrations: [react()],
   output: 'static',
   adapter: netlify(),
-  build: {
-    inlineStylesheets: 'auto'
-  },
   vite: {
     build: {
       sourcemap: true
